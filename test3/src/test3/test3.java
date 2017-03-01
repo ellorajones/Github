9afed64cdbf759f1302ff2efd78bee5a0b2944c4
Ellora <Ellora@Ellora-PC1>
2017-03-01 12:12:08 +0530

    this is test3 for new1 branch

---
+++ test3/src/test3/test3.java
@@ -0,0 +1,10 @@
+package test3;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test3 tested:is Added in branch 2");
+	}
+
+}
