02df0af9ab8e9539f60df8fe86e8be414b645292
Ellora <Ellora@Ellora-PC1>
2017-03-01 12:32:03 +0530

    edited

--- test0/src/test0/test0.java
+++ test0/src/test0/test0.java
@@ -5,6 +5,7 @@ public class test0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 0 is selected");
+		System.out.println("Edited to see change");
 	}
 
 }
